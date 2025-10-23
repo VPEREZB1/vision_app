1540d6295c3301b7796b50907efd93c644a6e618
VPEREZB1 <vperezb1@eafit.edu.co>
2025-10-23 00:48:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def encode_image(image_file):
 
 st.set_page_config(page_title="Analisis de imagen", layout="centered", initial_sidebar_state="collapsed")
 # Streamlit page setup
-st.title("Análisis de Imagen:🤖🏞️")
+st.title("🏞️Análisis de Imagen:🏞️")
 ke = st.text_input('Ingresa tu Clave')
 os.environ['OPENAI_API_KEY'] = ke
 
